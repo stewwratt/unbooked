42c3a16a85b3fa5883c59e30e3a012e907d7343e
stewwratt <92232004+stewwratt@users.noreply.github.com>
2024-01-01 16:01:06 +1100

    Initialised firebase firestore database and build new svelte project template

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "your-api-key",
+  authDomain: "your-project-id.firebaseapp.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project-id.appspot.com",
+  messagingSenderId: "your-sender-id",
+  appId: "your-app-id",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const db = firebase.firestore();
